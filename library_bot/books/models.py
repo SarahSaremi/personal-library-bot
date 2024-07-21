04dc56111aa93d60f962c5e70620faf6477718f8
sar_mi <sarasaaremi@gmail.com>
2024-07-21 15:30:13 +0330

    add book model

--- library_bot/books/models.py
+++ library_bot/books/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    writer = models.CharField(max_length=100)
+    translator = models.CharField(max_length=100, null=True, blank=True)
+    borrower = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.title
